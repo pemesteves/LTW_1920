30d7b10c8505f8111887a10034e9afbeec22c3c0
Pedro Esteves <up201705160@fe.up.pt>
2019-11-13 08:50:41 +0000

    Changed solutions

--- PHP_exercises/exercise3/news_item.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$stmt = $db->prepare('SELECT * FROM news JOIN users USING (username) WHERE id = :id');
-  	$stmt->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
-  	$stmt->execute();
-  	$article = $stmt->fetch();
- 
-?>
